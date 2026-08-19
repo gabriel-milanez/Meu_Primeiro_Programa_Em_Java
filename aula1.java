75f643db75a7bbf643326272998241de9b558fee
gabriel-milanez <gabriel.milanez@escola.pr.gov.br>
2026-08-19 15:33:13 +0000

    JAVA I

---
+++ aula1.java
@@ -0,0 +1,11 @@
+public class aula1 {
+    
+    public static void main(String[] args) {
+
+        System.out.println("___Olá, seja bem vindo___");
+        System.out.println("Digite 1 para sacar");
+        System.out.println("Digite 2 para depositar");
+        System.out.println("Digite 3 para ver o estrato");
+        System.out.println("Digite 4 para sair");
+    }
+}
