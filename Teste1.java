82088fb1cb98becf4ffc044afad3848ebde4440a
gabriel-milanez <gabriel.milanez@escola.pr.gov.br>
2026-09-11 12:41:06 +0000

    teste1

---
+++ Teste1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Teste1 {
+
+    public static void main(String[] args) {
+        ArrayList<String> carrinho = new ArrayList<>();
+        carrinho.add("Notebook");
+        carrinho.add("Mouse");
+        carrinho.add("Teclado");
+        carrinho.add("Monitor");
+        carrinho.add("Caixa de som");
+        carrinho.add("Energético");
+        carrinho.add("Controle");
+        carrinho.add("Mousepad");
+        carrinho.add("Placa de video");
+        carrinho.add("SSD");
+
+        System.out.println("Bem vindo a loja!");
+        System.out.println("O que você quer fazer?")
+    }
+}
